75d58b0a0c05dda4a46fc5199bb082696bd2ac28
JaimeSolisS <jaimesolissoto@me.com>
2022-01-21 20:09:02 -0700

    update home page

--- pages/home.py
+++ pages/home.py
@@ -2,9 +2,18 @@ import streamlit as st
 
 def app():
     st.markdown("""
-    Hey there! Welcome to Jaime's Twitter Sentiment Analysis App. This app
-    parses (never keeps or stores!) the tweets fetched from the Twitter API
-    and analyzes data about a specific user or tweets that contain a specific word, phrase or hashtag. 
+    Hi! Welcome to my Twitter Sentiment Analysis App. This app
+    parses the tweets from **a specific user** or tweets that contain **a specific word, phrase or hashtag** including:   
+    
+    - Polarity and Subjectivity 
+    - Classification by Sentiment (Positive, Neutral or Negative)
+    - Classification by Type (Organic, Quotes, Replies and RTs) 
+    - Most Frequent Words used
+
+    After these graphs, you will find some samples of positive and negative tweets.  
+
+    All this data comes from the Twitter API and none is stored by this app 😉.
+   
     """)
 
  
@@ -13,4 +22,4 @@ def app():
     to determine whether the writer’s attitude towards a particular topic, product, 
     etc. is positive, negative, or neutral.”— Oxford English Dictionary* ''')
   
-    st.write("**👈 To begin, plese select a different page from the sidebar.**")
+    st.write("👈 To begin, plese select either **User** or **Search** from the sidebar.")
